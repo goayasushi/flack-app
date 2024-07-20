cd3d5e90ede04ba49cae80fdc706a4e1fbc76890
goayasushi <goayasushi@gmail.com>
2024-07-20 10:39:49 +0900

    テンプレート　for文

--- hello.py
+++ hello.py
@@ -3,6 +3,15 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/japan/<city>")
-def hello_world(city):
-    return render_template("hello.html",city = city)
+bullets = [
+    "箇条書き1",
+    "箇条書き2",
+    "箇条書き3",
+    "箇条書き4",
+    "箇条書き5",
+    "箇条書き6",
+]
+
+@app.route("/")
+def hello_world():
+    return render_template("hello.html",bullets = bullets)
