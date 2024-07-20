098d2b73f20a0951f0357259a40147945f111b46
goayasushi <goayasushi@gmail.com>
2024-07-20 10:19:06 +0900

    htmlを変数として返却

--- hello.py
+++ hello.py
@@ -2,6 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+html = '''
+<h1>サンプルHTML</h1>
+<ul>
+<li>箇条書き1</li>
+<li>箇条書き2</li>
+<li>箇条書き3</li>
+</ul>
+'''
+
 @app.route("/")
 def hello_world():
-    return "<h1>Hello World!</h1>"
+    return html
