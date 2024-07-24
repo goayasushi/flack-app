fd4fda5208c76a81d0ed92b2593414e98167f281
goayasushi <goayasushi@gmail.com>
2024-07-25 08:47:52 +0900

    削除機能

--- app.py
+++ app.py
@@ -59,3 +59,11 @@ def update(id):
 
         db.session.commit()
         return redirect("/")
+    
+@app.route("/<int:id>/delete", methods=["GET"])
+def delete(id):
+    post = Post.query.get(id)
+    
+    db.session.delete(post)
+    db.session.commit()
+    return redirect("/")
