824c58963054a3740dcd89b064c875a703da9b53
goayasushi <goayasushi@gmail.com>
2024-07-25 17:25:22 +0900

    ログイン、サインアップ、ログアウト機能

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, LoginManager
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from datetime import datetime
 import pytz
@@ -36,16 +37,55 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(30), unique=True)
     password = db.Column(db.String(12))
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 with app.app_context():
     db.create_all()
 
 @app.route("/", methods=["GET", "POST"])
+@login_required
 def index():
     if request.method == "GET":
         posts = Post.query.order_by(Post.created_at.desc()).all()
         return render_template("index.html", posts = posts)
 
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = User(username=username, password=generate_password_hash(password, "sha256"))
+
+        db.session.add(user)
+        db.session.commit()
+        return redirect("/login")
+    else:
+        return render_template("signup.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(username=username).first()
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect("/")
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect("/login")
+
 @app.route("/create", methods=["GET", "POST"])
+@login_required
 def create():
     if request.method == "POST":
         title = request.form.get("title")
@@ -60,6 +100,7 @@ def create():
         return render_template("create.html")
     
 @app.route("/<int:id>/update", methods=["GET", "POST"])
+@login_required
 def update(id):
     post = Post.query.get(id)
     if request.method == "GET":
@@ -72,6 +113,7 @@ def update(id):
         return redirect("/")
     
 @app.route("/<int:id>/delete", methods=["GET"])
+@login_required
 def delete(id):
     post = Post.query.get(id)
     
